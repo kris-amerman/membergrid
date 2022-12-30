c43ef78989fc9ccda83cc83f1e80a816eb678ab2
Kris Amerman <amerman.k@northeastern.edu>
2022-12-30 01:21:05 -0700

    eliminate firebase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,5 @@
 import { useEffect, useState } from 'react';
-
-import { collection, getDocs } from "firebase/firestore";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
-import { useAuthState } from 'react-firebase-hooks/auth'; 
 import React from 'react';
-
-import { app, auth, provider, db } from './firebase';
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { MemberProvider, useMembers } from "./store";
@@ -50,63 +43,27 @@ const MemberCards = () => {
 function App() {
 
   const SignOut = () => {
-    return auth.currentUser && (
-      <button onClick={() => auth.signOut()}>Sign out</button>
+    // auth.currentUser &&
+    return (
+      <button onClick={() => {}}>Sign out</button>
     )
   } 
 
   const signInWithGoogle = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        // Gives a Google Access Token to access the Google API 
-        const credential = GoogleAuthProvider.credentialFromResult(result);
-        if (credential == null) {
-          throw new Error(
-            `Expected an OAuthCredential but received null.` + 
-            `Could not extract the underlying OAuthCredential` + 
-            `from the provided UserCredential: ${result}`);
-        }
-        const token = credential.accessToken;
-        // The signed-in user info
-        const user = result.user;
-        // ... REDIRECT
-        
-      }).catch((error) => {
-        // Handle Errors
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used
-        const email = error.customData.email;
-        // The AuthCredential type that was used
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        // ... 
-      });
+    // ...
   }
 
-  const [user, loading, error] = useAuthState(auth);
+  // const [user, loading, error] = useAuthState(auth);
   const [members, setMembers] = useState<{ id: string; }[]>([]);
 
   const fetchMembers = async () => {
 
-    await getDocs(collection(db, "members"))
-      .then((querySnapshot) => {
-        const newData = querySnapshot.docs.map(
-          (doc) => ({ ...doc.data(), id: doc.id })
-        );
-        setMembers(newData);
-        console.log(newData);
-      })
-
   }
-
-  useEffect(() => {
-    fetchMembers();
-  }, [user])
 
   return (
     <div>
-      {user ? <h1>USER</h1> : <h1>NO USER</h1>}
+      {/* {user ? <h1>USER</h1> : <h1>NO USER</h1>} */}
       <button onClick={signInWithGoogle}>Login</button>
       <SignOut />
       <div>
